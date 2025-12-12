f84eb19a417b1a155fdc9df49e804720bb224eeb
Kerman-Ramos <kramosarruti@gmail.com>
2025-12-12 18:54:03 +0100

    Update Evento.java

--- BattleRoyale/defaultPackage/Evento.java
+++ BattleRoyale/defaultPackage/Evento.java
@@ -1,17 +1,28 @@
 package defaultPackage;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Evento {
+public class Evento extends ListaArmas {
 	private static final Integer RONDA=1;
-	
 	
 	private List<Personaje> jugadores;
 	
+	public Evento(List<Personaje> jugadores) {
+		super();
+		this.jugadores=jugadores;
+	}
 	
 	
 	
 	
-	
-	
+	public static void main(String[] args) {
+		List<Personaje> jugadores=new ArrayList<Personaje>();
+		jugadores.add(new Personaje());
+		jugadores.add(new Personaje());
+		jugadores.add(new Personaje());
+		jugadores.add(new Personaje());
+		
+		System.out.println(jugadores.get(0));
+	}
 }
