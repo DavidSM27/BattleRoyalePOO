e4bb993bbd4c10a312a7626a7bfdad839420ac4f
Kerman-Ramos <kramosarruti@gmail.com>
2025-12-12 17:05:17 +0100

    Update Evento.java

--- BattleRoyale/defaultPackage/Evento.java
+++ BattleRoyale/defaultPackage/Evento.java
@@ -1,5 +1,17 @@
 package defaultPackage;
 
+import java.util.List;
+
 public class Evento {
+	private static final Integer RONDA=1;
+	
+	
+	private List<Personaje> jugadores;
+	
+	
+	
+	
+	
+	
 	
 }
