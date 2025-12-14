cccd14d55f96e05a4da235a9e413b165a170431a
Kerman-Ramos <kramosarruti@gmail.com>
2025-12-14 17:06:47 +0100

    Update Equipo.java

--- BattleRoyale/defaultPackage/Equipo.java
+++ BattleRoyale/defaultPackage/Equipo.java
@@ -62,4 +62,13 @@ public class Equipo {
 		return sonNPCs;
 	}
 	
+	public int getMediaSuerte() {
+		Integer media=0;
+		for(int i=0; i<miembros.size(); i++) {
+			media+=miembros.get(i).getSuerte();
+		}
+		
+		return media/miembros.size();
+	}
+	
 }
