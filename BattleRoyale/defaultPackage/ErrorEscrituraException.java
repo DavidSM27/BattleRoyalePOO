89e710b55f6eb47a5a179a80246ad5377eb7f922
Kerman-Ramos <kramosarruti@gmail.com>
2025-12-14 15:31:15 +0100

    Create ErrorEscrituraException.java

---
+++ BattleRoyale/defaultPackage/ErrorEscrituraException.java
@@ -0,0 +1,10 @@
+package defaultPackage;
+
+public class ErrorEscrituraException extends Exception {
+	
+	public ErrorEscrituraException(String mensaje, Throwable causaOriginal) {
+		super(mensaje, causaOriginal);
+	}
+	
+	
+}
