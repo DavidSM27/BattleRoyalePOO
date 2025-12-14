791f2f164c3d6db9d6ff52dd12099d0df0d25f5e
davutad <david.san@live.u-tad.com>
2025-12-14 12:58:53 +0100

    clase batalla

--- BattleRoyale/defaultPackage/Batalla.java
+++ BattleRoyale/defaultPackage/Batalla.java
@@ -5,56 +5,72 @@ import java.util.List;
 
 public abstract class Batalla {
 
-		protected Object ganador;
-		protected Object perdedor;
-		protected boolean enCurso;
-		
-		public void iniciar() {
-			enCurso = true;
-	        ganador = null;
-	        perdedor = null;
-	        System.out.println("La batalla ha comenzado");
-		}
-		
-		public String obtenerInfo() {
-			String estado = null;
-			String fraseGanador=null, frasePerdedor=null;
-			if(enCurso==true) {
-				estado="EN CURSO";
-			}else {
-				estado="FINALIZADA";
-			}
-			
-			if (ganador != null) {
-			    fraseGanador=ganador.toString();
-			    frasePerdedor=perdedor.toString();
-			} else {
-				fraseGanador="Aún no hay";
-				frasePerdedor="Aún no hay";
-			}
+	protected Object ganador;
+	protected Object perdedor;
+	protected boolean enCurso;
 
-	        return "Estado de la batalla: " + estado + "\nGanador: " + fraseGanador + "\nPerdedor: " + frasePerdedor;
+	public Batalla() {
+		this.ganador = null;
+		this.perdedor = null;
+		this.enCurso = false;
+	}
+
+	public void iniciar() {
+		enCurso = true;
+		ganador = null;
+		perdedor = null;
+		System.out.println("\n ========== LA BATALLA HA COMENZADO ========== \n");
+	}
+
+	public String obtenerInfo() {
+		String estado;
+		if (enCurso == true) {
+			estado = "EN CURSO";
+		} else {
+			estado = "FINALIZADA";
 		}
-		
-		public java.util.List<String> obtenerAcciones(){
-			List<String> acciones = new ArrayList<>();
-	        acciones.add("Atacar");
-	        acciones.add("Defender");
-	        acciones.add("Pasar turno");
-	        return acciones;
+
+		String fraseGanador, frasePerdedor;
+		if (ganador != null) {
+			fraseGanador = ganador.toString();
+			frasePerdedor = perdedor.toString();
+		} else {
+			fraseGanador = "Aún no hay";
+			frasePerdedor = "Aún no hay";
 		}
-		
-		public Object obtenerResultado() {
-			if (ganador != null) {
-	            System.out.println("La batalla ha terminado. Ganador: " + ganador);
-	        } else {
-	            System.out.println("La batalla aún no tiene ganador");
-	        }
-	        return ganador;
+
+		return "Estado de la batalla: " + estado + "\nGanador: " + fraseGanador + "\nPerdedor: " + frasePerdedor;
+	}
+
+	public List<String> obtenerAcciones() {
+		List<String> acciones = new ArrayList<>();
+		acciones.add("Atacar");
+		acciones.add("Defender");
+		acciones.add("Pasar turno");
+		return acciones;
+	}
+
+	public Object obtenerResultado() {
+		if (ganador != null) {
+			System.out.println("La batalla ha terminado. Ganador: " + ((Personaje) ganador).getNombre());
+		} else {
+			System.out.println("La batalla aún no tiene ganador");
 		}
-		
-		public boolean estaEnCurso() {
-	        return enCurso;
-	    }
-		
+		return ganador;
+	}
+
+	public boolean estaEnCurso() {
+		return enCurso;
+	}
+
+	protected void terminar(Personaje ganador, Personaje perdedor) {
+		this.enCurso = false;
+		this.ganador = ganador;
+		this.perdedor = perdedor;
+
+		System.out.println("\n ========== BATALLA TERMINADA ==========");
+		System.out.println("Ganador: " + ganador.getNombre());
+		System.out.println("Perdedor: " + perdedor.getNombre());
+		System.out.println("==========================================\n");
+	}
 }
