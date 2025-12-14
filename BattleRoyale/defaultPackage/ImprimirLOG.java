70b5b82f5b538da1f220e0f49c32960c9f37f9b9
Kerman-Ramos <kramosarruti@gmail.com>
2025-12-14 13:43:45 +0100

    Create ImprimirLOG.java

---
+++ BattleRoyale/defaultPackage/ImprimirLOG.java
@@ -0,0 +1,16 @@
+package defaultPackage;
+
+public class ImprimirLOG {
+	
+	public static final String RUTA_FICHERO="\\files\\Nombres_Jugadores.csv";
+	
+	public ImprimirLOG() {
+		
+	}
+	
+	public void imprimir(String log) {
+		
+	}
+	
+	
+}
