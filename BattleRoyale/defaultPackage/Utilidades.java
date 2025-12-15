53ec6ac0c9fce90de0b6ad89bebfffa3c6da66b7
Kerman-Ramos <kramosarruti@gmail.com>
2025-12-15 10:36:57 +0100

    Update Utilidades.java

--- BattleRoyale/defaultPackage/Utilidades.java
+++ BattleRoyale/defaultPackage/Utilidades.java
@@ -10,7 +10,6 @@ public class Utilidades {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.out.println("\nHilo interrumpido");
-                return;
             }
         }
         System.out.println(); // salto de línea al final
